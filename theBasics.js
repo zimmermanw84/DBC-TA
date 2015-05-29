8c6fb14d3430dfca83239cd0c84a3df842c91497
Walter Zimmerman <zimmermanw84@gmail.com>
2015-05-29 15:29:28 -0700

    version one theBasics

--- theBasics.js
+++ theBasics.js
@@ -22,7 +22,7 @@ var someOuterFunction = function() {
 
 };
 
-foo // => undefined
+console.log(foo) // => undefined
 
 // -----------------------------------------------
 
@@ -31,13 +31,10 @@ var foo = "foo"
 var someOuterFunction = function() {
 
   var someFunction = function() {
-    return foo // => "foo"
+    console.log(foo) // => "foo"
   }
 
 };
-
-// SIDE_NOTE: There is no implicit return in JS you must use the keyword "return" if you
-// want your function to return something!
 
 // When someFunction sees the foo it first checks within someFunction's (its own) scope
 // asks "you ever heard of the variable foo?" someFunction replies to itself "Nope, never
@@ -49,3 +46,69 @@ var someOuterFunction = function() {
 // A contrived example, sure, however this illustrates how it works when someFunction fires
 // it goes looking for foo. In the first example global scope is looking for foo and cannot
 // ask any outer functions nor is it defined locally and therefore it's undefined.
+
+
+// Function Declaration vs Function Expression(s)
+
+// Function Declaration
+
+// Will be hoisted
+function foo() {
+  return "foo";
+};
+
+// Won't be hoisted
+
+// Anonymous Function Expression
+var foo = function() {
+  return "foo";
+};
+
+// Named Function Expression
+var foo = function foo() {
+  return "foo";
+};
+
+// -----------------------------------------------
+
+foo(); // => "foo"
+
+function foo() {
+  return "foo";
+};
+
+// -----------------------------------------------
+
+foo(); // => undefined
+
+var foo = function() {
+  return "foo";
+};
+
+// Now why is that? That's right... Hoisting.
+// Without going into too much detail, it's the "var" declaration that keeps the function
+// expression in place. It's actually the same reason when you declare a variable without
+// "var" it ends up on the global scope.
+
+// So what's the big deal?
+
+// -----------------------------------------------
+
+foo = 1;
+
+var someFunction = function() {
+  foo = "bar";
+}
+
+test();
+console.log(foo) // => "bar";
+
+// On a bigger codebase with multiple developers working on the same app. You don't have
+// control over other people's variable name choices. What if you get a bug where you're
+// overriding someone else's code and it's takes you years to find it? Or one of your
+// functions get hoisted during assest compiling and breaks production?
+
+// Sure that sounds extream but it's possible. The only thing we actually have in life
+// is time. If you can save yourself some before you spend it on fixing a problem
+// that was completely preventable... You'll have more time to spend all that money
+// you're making... ;P
